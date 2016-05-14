58bf3896612654778a7e61451190e3c271ad5d78
m4bwav <m4bwav@gmail.com>
2016-05-14 12:42:20 -0500

    Fix smoke test.

--- test.js
+++ test.js
@@ -2,8 +2,8 @@ import test from 'ava';
 import linkReplacer from './';
 
 test.cb('Basic link replacement', function (t) {
-  linkReplacer.replacePlainLinks("http://www.google.com", function (newMarkdown) {
-    t.is(newMarkdown, '[Google], *google.com*');
+  linkReplacer.replacePlainLinks('http://www.google.com', function (newMarkdown) {
+    t.is(newMarkdown, '"[Google](http://www.google.com)", *google.com*');
     t.end();
   });
 });
