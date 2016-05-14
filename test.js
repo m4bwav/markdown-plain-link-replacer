b2a0651f9272eeaa74ff4ca691c5da1bb211b437
m4bwav <m4bwav@gmail.com>
2016-05-14 12:46:08 -0500

    Added promise reference.

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 import test from 'ava';
 import linkReplacer from './';
 
+global.Promise = Promise;
+
 test.cb('Basic link replacement', function (t) {
   linkReplacer.replacePlainLinks('http://www.google.com', function (newMarkdown) {
     t.is(newMarkdown, '"[Google](http://www.google.com)", *google.com*');
